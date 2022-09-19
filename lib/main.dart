32e29a592dc91b50ffb3885eacf1b0be3080d2c5
osykris <osy.krisdayanti27@gmail.com>
2022-09-19 10:56:37 +0700

    Perspective flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,46 +2,24 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.yellow,
       ),
-      home: const MyHomePage(title: 'Praktikum Osy'),
+      home: const MyHomePage(title: 'Tugas Osy'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,20 +28,33 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Offset _offset = Offset.zero; // changed
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    return Transform(
+      // Transform widget
+      transform: Matrix4.identity()
+        ..setEntry(3, 2, 0.001) // perspective
+        ..rotateX(0.01 * _offset.dy) // changed
+        ..rotateY(-0.01 * _offset.dx), // changed
+      alignment: FractionalOffset.center,
+      child: GestureDetector(
+        // new
+        onPanUpdate: (details) => setState(() => _offset += details.delta),
+        onDoubleTap: () => setState(() => _offset = Offset.zero),
+        child: _defaultApp(context),
+      ),
+    );
+  }
+
+  _defaultApp(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -145,7 +136,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: const Text("multiple transformations (matrix)"),
                 onPressed: () {},
               ),
-            )
+            ),
           ],
         ),
       ),
